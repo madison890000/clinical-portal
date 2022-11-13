e2eb341ef009b564d67cdd55642bfcb526ef4d83
madison890000 <madison.sss.ma@gmail.com>
2022-11-13 12:10:42 +0800

    using useReducer to manage AppStore

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,72 +1,16 @@
-import React, {
-    Dispatch,
-    PropsWithChildren,
-    SetStateAction,
-    useCallback,
-    useEffect,
-    useState
-} from 'react';
+import React, { PropsWithChildren } from 'react';
 import { IClinician, LoginStatus, PatientSummary } from '../types';
-import { getClinicianInfo, getPatientList, login } from '../services';
-import { SESSION_TOKEN_SESSION_STORAGE_KEY } from '../constants';
+import useAppStore from './useAppStore';
 
 interface IAppContext {
     loginStatus: LoginStatus;
     login: (username: string, password: string) => Promise<void>;
     logout: () => Promise<void>;
-    setLoginStatus: Dispatch<SetStateAction<LoginStatus>>;
     clinician?: IClinician;
     patients?: PatientSummary[];
 }
 
 export const AppContext = React.createContext({} as IAppContext);
-
-
-const useAppStore = () => {
-    const [loginStatus, setLoginStatus] = useState<LoginStatus>(LoginStatus.NotLogin);
-    const [clinician, setClinician] = useState<IClinician | undefined>();
-    const [patients, setPatients] = useState<PatientSummary[]>([]);
-    const getClinicianDetail = useCallback(async () => {
-        const info = await getClinicianInfo();
-        setClinician(info);
-    }, [setClinician]);
-    const getPatients = useCallback(async () => {
-        const list = await getPatientList();
-        setPatients(list?.patients);
-    }, [setPatients]);
-    const loginHandle = useCallback(async (username: string, password: string) => {
-        await login(username, password);
-        window.notificator('Login Success', 'success');
-        setLoginStatus(LoginStatus.Login);
-    }, [setLoginStatus]);
-    const logout = useCallback(async () => {
-        setLoginStatus(LoginStatus.NotLogin);
-        setPatients([]);
-        setClinician(undefined);
-        window.notificator('Logout Success', 'success');
-    }, [setLoginStatus]);
-    useEffect(() => {
-        if (loginStatus === LoginStatus.Login) {
-            getClinicianDetail();
-            getPatients();
-        }
-    }, [loginStatus, getClinicianDetail, getPatients]);
-
-    useEffect(() => {
-        if (window.sessionStorage.getItem(SESSION_TOKEN_SESSION_STORAGE_KEY)) {
-            setLoginStatus(LoginStatus.Login);
-        }
-    }, []);
-    return {
-        loginStatus,
-        login: loginHandle,
-        logout,
-        setLoginStatus,
-        clinician,
-        patients,
-    }
-}
-
 
 export const AppContextContainer = ({ children }: PropsWithChildren) => {
     const {
@@ -74,7 +18,6 @@ export const AppContextContainer = ({ children }: PropsWithChildren) => {
         clinician,
         login,
         logout,
-        setLoginStatus,
         patients,
     } = useAppStore();
     return (
@@ -83,7 +26,6 @@ export const AppContextContainer = ({ children }: PropsWithChildren) => {
                 loginStatus,
                 login,
                 logout,
-                setLoginStatus,
                 clinician,
                 patients,
             }}
---
+++ src/contexts/useAppStore.tsx
@@ -0,0 +1,117 @@
+import React, { useCallback, useEffect, useReducer } from 'react';
+import { IClinician, LoginStatus, PatientSummary } from '../types';
+import { getClinicianInfo, getPatientList, login } from '../services';
+import { SESSION_TOKEN_SESSION_STORAGE_KEY } from '../constants';
+
+
+type State = {
+    loginStatus: LoginStatus;
+    clinician?: IClinician;
+    patients: PatientSummary[];
+}
+
+type ChangeLoginStatusAction = {
+    type: 'ChangeLoginStatus';
+    payload: LoginStatus;
+}
+type UpdateClinicianAction = {
+    type: 'UpdateClinician';
+    payload?: IClinician;
+}
+type UpdatePatientsAction = {
+    type: 'UpdatePatients';
+    payload: PatientSummary[];
+}
+type LogoutAction = {
+    type: 'Logout';
+}
+
+type Action = ChangeLoginStatusAction | UpdateClinicianAction | UpdatePatientsAction | LogoutAction;
+
+const INITIAL_STATE = {
+    loginStatus: LoginStatus.NotLogin,
+    patients: []
+}
+const reducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case 'ChangeLoginStatus':
+            return {
+                ...state,
+                loginStatus: action.payload,
+            }
+        case 'UpdateClinician':
+            return {
+                ...state,
+                clinician: action.payload,
+            }
+        case 'UpdatePatients':
+            return {
+                ...state,
+                patients: action.payload,
+            }
+        case 'Logout':
+            return {
+                ...INITIAL_STATE,
+            }
+        default:
+            return state
+    }
+}
+
+
+const useAppStore = () => {
+    const [{ loginStatus, clinician, patients }, dispatch] = useReducer(reducer, INITIAL_STATE);
+    const getClinicianDetail = useCallback(async () => {
+        const info = await getClinicianInfo();
+        dispatch({
+            type: 'UpdateClinician',
+            payload: info
+        });
+    }, [dispatch]);
+    const getPatients = useCallback(async () => {
+        const list = await getPatientList();
+        dispatch({
+            type: 'UpdatePatients',
+            payload: list?.patients ?? []
+        });
+    }, [dispatch]);
+    const loginHandle = useCallback(async (username: string, password: string) => {
+        await login(username, password);
+        window.notificator('Login Success', 'success');
+        dispatch({
+            type: 'ChangeLoginStatus',
+            payload: LoginStatus.Login
+        });
+    }, [dispatch]);
+    const logout = useCallback(async () => {
+        dispatch({
+            type: 'Logout',
+        });
+        window.notificator('Logout Success', 'success');
+    }, [dispatch]);
+    useEffect(() => {
+        if (loginStatus === LoginStatus.Login) {
+            getClinicianDetail();
+            getPatients();
+        }
+    }, [loginStatus, getClinicianDetail, getPatients]);
+
+    useEffect(() => {
+        if (window.sessionStorage.getItem(SESSION_TOKEN_SESSION_STORAGE_KEY)) {
+            dispatch({
+                type: 'ChangeLoginStatus',
+                payload: LoginStatus.Login
+            });
+        }
+    }, []);
+    return {
+        loginStatus,
+        login: loginHandle,
+        logout,
+        clinician,
+        patients,
+    }
+}
+
+
+export default useAppStore
