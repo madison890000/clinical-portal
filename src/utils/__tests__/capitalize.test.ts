06ff218ed55a63ed4e3085fc13a755c6f0c9f609
madison890000 <madison.sss.ma@gmail.com>
2022-11-13 12:08:59 +0800

    add test case for capitalize

---
+++ src/utils/__tests__/capitalize.test.ts
@@ -0,0 +1,14 @@
+import { capitalize } from '../index';
+
+describe('utils -----> capitalize ', () => {
+    test('capitalize: make first letter to be uppercase', () => {
+        expect(capitalize('test'))
+            .toBe('Test');
+        expect(capitalize())
+            .toBe('');
+        expect(capitalize('abcs'))
+            .toBe('Abcs');
+        expect(capitalize('abcs def'))
+            .toBe('Abcs def');
+    });
+});
