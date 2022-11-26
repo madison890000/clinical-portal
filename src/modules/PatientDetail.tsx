6a6a7463928b408eb4511ce31edda82ade7eff0e
madison890000 <madison.sss.ma@gmail.com>
2022-11-27 00:19:21 +0800

    feature: use react query to fetch data

--- src/modules/PatientDetail.tsx
+++ src/modules/PatientDetail.tsx
@@ -1,25 +1,13 @@
 import Name from '../components/Name';
-import { useCallback, useEffect, useState } from 'react';
-import { getPatientById } from '../services';
-import { IPatient } from '../types';
 import { Card, Skeleton } from '@mui/material';
 import InfoItem from '../components/InfoItem';
 import styles from './PatientDetail.module.scss';
+import { useQuery } from 'react-query';
+import { getPatientById } from '../services';
 
 const PatientDetail = ({ id }: { id: string }) => {
-    const [patient, setPatient] = useState<IPatient>();
-    const getPatientDetail = useCallback(
-        async (patientId: string) => {
-            const detail = await getPatientById(patientId);
-            setPatient(detail);
-        },
-        [setPatient]
-    );
-    useEffect(() => {
-        if (id) {
-            getPatientDetail(id);
-        }
-    }, [id, getPatientDetail]);
+    const { data: patient } = useQuery(['getPatientDetail', id], () => getPatientById(id));
+
     return (
         <>
             {!patient && <Skeleton variant="rectangular" width={800} height={118} />}
